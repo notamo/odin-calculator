672482999a1b214b2fa5b5ca23ca6c88c209a093
notamo <203415281+notamo@users.noreply.github.com>
2025-11-08 20:41:45 +0100

    Draft operate() function

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+const operand1 = 0;
+const operand2 = 0;
+const operator = "+";
+
+function operate(operand1 = 0, operand2 = 0, operator = add) {
+    console.log(operator(operand1, operand2));
+}
+
+operate(3, 5, multiply);
