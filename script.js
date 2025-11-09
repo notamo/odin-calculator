b2dc59571423d6d0431c10522938731b29819933
notamo <203415281+notamo@users.noreply.github.com>
2025-11-09 21:02:48 +0100

    Implement logic that allows single calculation
    Now when the calculator actually does something, it is clear that
    JavaScript objects are well suited for this project. Refactoring the
    code to utilize objects is the next step.

--- script.js
+++ script.js
@@ -14,12 +14,69 @@ function divide(a, b) {
     return a / b;
 }
 
-const operand1 = 0;
-const operand2 = 0;
-const operator = add;
-
 function operate(operand1 = 0, operand2 = 0, operator = add) {
-    console.log(operator(operand1, operand2));
+    const result = operator(operand1, operand2);
+    return result;
 }
 
-operate(3, 5, multiply);
+function populateDisplay(str) {
+    display.textContent = str;   
+}
+
+let operand1;
+let operand2;
+let operatorText;
+
+const display = document.querySelector(".display p");
+
+const keypad = document.querySelector(".keypad");
+keypad.addEventListener("click", (e) => {
+    console.log("Button was pressed");
+    
+    const targetClass = e.target.className;
+    const targetText = e.target.innerText;
+    
+    if (targetClass === "digit") {
+        console.log(`Digit ${targetText} was pressed`);
+
+        if (!operand1 || (!operatorText && !operand2)) {
+            operand1 = parseInt(targetText);
+            populateDisplay(`${operand1}`);
+        } else if (!operand2 && operatorText) {
+            operand2 = parseInt(targetText);
+            populateDisplay(`${operand1}${operatorText}${operand2}`)
+        }
+    } else if (targetClass === "operator") {
+        console.log(`Operator ${targetText} was pressed`);
+
+        if (operand1 && !operatorText) {
+            operatorText = targetText;
+            populateDisplay(`${operand1}${operatorText}`)
+        }
+    } else if (e.target.id === "result") {
+        console.log("= button was clicked");
+
+        if (operand1 && operand2 && operatorText) {
+            const result = operate(operand1, operand2, opTextToFunc[operatorText]);
+            populateDisplay(`${result}`);
+            resetState();
+        }   
+    } else if (e.target.id === "clear") {
+        console.log("AC button was clicked");
+        populateDisplay("");
+        resetState();
+    } 
+});
+
+function resetState() {
+    operand1 = null;
+    operand2 = null;
+    operatorText = null;
+}
+
+const opTextToFunc = {
+    "+": add,
+    "−": subtract,
+    "×": multiply,
+    "÷": divide,
+}
