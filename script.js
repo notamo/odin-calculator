c1357e92e5a7c08be0a98121e3ae6a6d9d511941
notamo <203415281+notamo@users.noreply.github.com>
2025-11-10 00:34:31 +0100

    Implement working calculator

--- script.js
+++ script.js
@@ -1,82 +1,117 @@
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(operand1 = 0, operand2 = 0, operator = add) {
-    const result = operator(operand1, operand2);
-    return result;
-}
-
-function populateDisplay(str) {
-    display.textContent = str;   
-}
-
-let operand1;
-let operand2;
-let operatorText;
-
-const display = document.querySelector(".display p");
-
-const keypad = document.querySelector(".keypad");
-keypad.addEventListener("click", (e) => {
-    console.log("Button was pressed");
+class Calculator {
     
-    const targetClass = e.target.className;
-    const targetText = e.target.innerText;
+    constructor(initVal = 0) {
+        this.firstOperand = null;
+        this.secondOperand = null;
+        this.operator = null;
+        this.replaceFirstOperand = false;
+
+        this.display = document.querySelector(".display p");
+        this.setDisplay(initVal);
+
+        this.keypad = document.querySelector(".keypad");
+        this.keypad.addEventListener("click", (e) => {
+
+            if (e.target.id === "ac") {
+                this.reset();
+            } else if (e.target.id === "result") {
+                this.operate();
+            } else if (e.target.className === "digit") {
+                this.handleDigit(e);
+            } else if (e.target.className === "operator") {
+                this.handleOperator(e);
+            }
+
+            console.log({x: this.firstOperand, op: this.operator, y: this.secondOperand, replace: this.replaceFirstOperand});
+        });
+    }
+
+    handleDigit(e) {
+        const btnText = e.target.innerText;
+
+        if (!this.firstOperand) {
+            this.firstOperand = btnText;
+        } else if (this.firstOperand && this.replaceFirstOperand) {
+            this.firstOperand = btnText;
+            this.replaceFirstOperand = false;
+        } else if (this.firstOperand && !this.operator && !this.secondOperand) {
+            this.firstOperand += btnText;
+        } else if (this.firstOperand && this.operator && !this.secondOperand) {
+            this.secondOperand = btnText;
+        } else {
+            this.secondOperand += btnText;
+        }
+
+        this.updateDisplay();
+    }
+
+    handleOperator(e) {
+        const btnText = e.target.innerText;
+
+        if (this.firstOperand && !this.operator) {
+            this.operator = btnText;
+        }
+
+        this.replaceFirstOperand = false;
+        this.updateDisplay();
+    }
+
+    operate() {
+        let res;
+        switch (this.operator) {
+            case "+":
+                res = this.add(this.firstOperand, this.secondOperand);
+                break;
+            case "−":
+                res = this.substract(this.firstOperand, this.secondOperand);
+                break;
+            case "×":
+                res = this.multiply(this.firstOperand, this.secondOperand);
+                break;
+            case "÷":
+                res = this.divide(this.firstOperand, this.secondOperand);
+                break;
+        }
     
-    if (targetClass === "digit") {
-        console.log(`Digit ${targetText} was pressed`);
+        this.reset();
+        this.firstOperand = `${res}`;
+        this.replaceFirstOperand = true;
+        this.updateDisplay();
+    }
 
-        if (!operand1 || (!operatorText && !operand2)) {
-            operand1 = parseInt(targetText);
-            populateDisplay(`${operand1}`);
-        } else if (!operand2 && operatorText) {
-            operand2 = parseInt(targetText);
-            populateDisplay(`${operand1}${operatorText}${operand2}`)
-        }
-    } else if (targetClass === "operator") {
-        console.log(`Operator ${targetText} was pressed`);
+    add(x, y) {
+        return Number(x) + Number(y);
+    }
 
-        if (operand1 && !operatorText) {
-            operatorText = targetText;
-            populateDisplay(`${operand1}${operatorText}`)
-        }
-    } else if (e.target.id === "result") {
-        console.log("= button was clicked");
+    substract(x, y) {
+        return Number(x) - Number(y);
+    }
 
-        if (operand1 && operand2 && operatorText) {
-            const result = operate(operand1, operand2, opTextToFunc[operatorText]);
-            populateDisplay(`${result}`);
-            resetState();
-        }   
-    } else if (e.target.id === "clear") {
-        console.log("AC button was clicked");
-        populateDisplay("");
-        resetState();
-    } 
-});
+    multiply(x, y) {
+        return Number(x) * Number(y);
+    }
 
-function resetState() {
-    operand1 = null;
-    operand2 = null;
-    operatorText = null;
+    divide(x, y) {
+        return Number(x) / Number(y);
+    }
+
+    reset() {
+        this.firstOperand = null;
+        this.secondOperand = null;
+        this.operator = null;
+        this.setDisplay(0);
+    }
+
+    updateDisplay() {
+        const op1 = this.firstOperand ? this.firstOperand : "";
+        const op2 = this.secondOperand ? this.secondOperand : "";
+        const op = this.operator ? this.operator : "";
+        this.setDisplay(op1 + op + op2);
+    }
+
+    setDisplay(str) {
+        this.display.textContent = `${str}`;
+    }
 }
 
-const opTextToFunc = {
-    "+": add,
-    "−": subtract,
-    "×": multiply,
-    "÷": divide,
-}
+const calc = new Calculator();
